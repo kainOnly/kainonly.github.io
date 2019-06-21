89bd45200511ae7a38e041e6085fe4435cdd5853
kain <zhangtqx@vip.qq.com>
2019-06-21 14:39:34 +0800

    Reset

--- controller/container.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "github.com/kataras/iris"
-
-type Container struct{}
-
-func (c *Container) ListsEndPoint(ctx iris.Context) {
-	ctx.JSON(iris.Map{})
-}
--- controller/image.go
+++
@@ -1,9 +0,0 @@
-package controller
-
-import "github.com/kataras/iris"
-
-type Image struct{}
-
-func (c *Image) ListsEndPoint(ctx iris.Context) {
-	ctx.JSON(iris.Map{})
-}
