9befcb2e0f93f76f878190421294f78e7b5587d0
kain <zhangtqx@vip.qq.com>
2019-08-26 20:30:45 +0800

    Submit UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import zh from '@angular/common/locales/zh';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
+registerLocaleData(zh);
 
 import { AppComponent } from './app.component';
-
 
 const routes: Routes = [];
 
@@ -13,9 +20,13 @@ const routes: Routes = [];
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    NgZorroAntdModule,
     RouterModule.forRoot(routes),
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
 })
 export class AppModule {
