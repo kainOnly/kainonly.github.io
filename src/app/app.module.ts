0ffdc1f18aa6148c80d483bda762f95332fba755
kain <zhangtqx@vip.qq.com>
2020-01-01 08:26:23 +0800

    更新布局

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,32 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {registerLocaleData} from '@angular/common';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import zh from '@angular/common/locales/zh';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
 
 registerLocaleData(zh);
 
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgZorroAntdModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    AppRoutingModule,
+    NgZorroAntdModule,
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [
+    {provide: NZ_I18N, useValue: zh_CN}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
